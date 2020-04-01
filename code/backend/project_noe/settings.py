968a906fdbc1b37534c0f365d2910e1eed173ed0
Zoltan Kohalmy <zoltan@getparkour.com>
2020-04-01 12:56:22 +0000

    Connect backend and nginx with docker compose

--- code/backend/project_noe/settings.py
+++ code/backend/project_noe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fake-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
