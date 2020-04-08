d3a1e43997f125c4ad71ff1d054440161ab8f125
Zoltan Kohalmy <zoltan@getparkour.com>
2020-04-08 15:01:02 +0000

    Fix model import

--- code/backend/users/serializers.py
+++ code/backend/users/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 
-from . import models as models
+from . import models as m
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = m.User
         fields = "__all__"
-
