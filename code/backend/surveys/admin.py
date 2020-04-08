caf0eb81a098e8c16e2db44de1b4b9f458a66489
Zoltan Kohalmy <zoltan@getparkour.com>
2020-04-08 13:42:23 +0000

    Add SurveyQuestion model and add it to admin

--- code/backend/surveys/admin.py
+++ code/backend/surveys/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models as m
+
+
+admin.site.register(m.SurveyQuestion)
--- code/backend/surveys/models.py
+++ code/backend/surveys/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class SurveyQuestion(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_active = models.BooleanField(default=False)
+    is_required = models.BooleanField(default=True)
+    answer_datatype = models.CharField(default='string')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created_at', )
+
