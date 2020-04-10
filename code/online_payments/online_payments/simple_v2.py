0a4c49c98773363a49250824354e27625aa8e394
Kiss György <kissgyorgy@me.com>
2020-04-10 19:24:32 +0200

    Implement response parsing for ease of use
    use dateutil for time formats

--- code/online_payments/online_payments/simple_v2.py
+++ code/online_payments/online_payments/simple_v2.py
@@ -1,18 +1,31 @@
 import base64
 import datetime as dt
+from dataclasses import dataclass
 import json
 import hashlib
 import hmac
 import secrets
 import requests
-
+from dateutil.parser import parse as dateutil_parse
 from . import exceptions
 
 
 START_PAYMENT_URL = "https://sandbox.simplepay.hu/payment/v2/start"
 
 
-def get_payment_url(
+@dataclass
+class StartPaymentResponse:
+    salt: str  # 32 character long random string
+    merchant: str  # SimplePay account, in which the transaction has been created
+    orderRef: str  # same as orderRef in start request
+    currency: str  # same as currency in start request
+    transaction_id: str  # created SimplePay transaction ID
+    timeout: dt.datetime  # time limit until payment can be started
+    total: float  # summary of the transaction
+    payment_url: str  # URL where we need to redirect the user
+
+
+def start_payment_request(
     *,
     merchant_id,
     secret_key,
@@ -39,8 +52,19 @@ def get_payment_url(
     # `requests`'s default json dumping would keep whitespaces
     # Data sent in request and used for generating signature must match
     headers = {"Signature": signature, "Content-Type": "application/json"}
-    rv = requests.post(START_PAYMENT_URL, json_request, headers=headers)
-    return rv.json()["paymentUrl"]
+    res = requests.post(START_PAYMENT_URL, json_request, headers=headers)
+    json_res = res.json()
+
+    return StartPaymentResponse(
+        salt=json_res["salt"],
+        merchant=json_res["merchant"],
+        orderRef=json_res["orderRef"],
+        currency=json_res["currency"],
+        transaction_id=json_res["transactionId"],
+        timeout=dateutil_parse(json_res["timeout"]),
+        total=json_res["total"],
+        payment_url=json_res["paymentUrl"],
+    )
 
 
 def _make_request(
