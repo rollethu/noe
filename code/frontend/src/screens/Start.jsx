6f10a0445662475e349ed27ab1aa0487d1f2c9bd
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 13:39:07 +0200

    Align Start screen text to center

--- code/frontend/src/screens/Start.jsx
+++ code/frontend/src/screens/Start.jsx
@@ -7,7 +7,7 @@ const TXT_CAPTION_TEXT = "Áthajtásos koronavírus (COVID-19) teszt";
 export default function Start() {
   return (
     <View>
-      <Caption>{TXT_CAPTION_TEXT}</Caption>
+      <Caption center>{TXT_CAPTION_TEXT}</Caption>
     </View>
   );
 }
