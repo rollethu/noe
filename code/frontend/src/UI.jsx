6c2b323d3c35ca140b3c3b5aa6abea8565d031c5
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 14:07:47 +0200

    Create reusable Field components

--- code/frontend/src/UI.jsx
+++ code/frontend/src/UI.jsx
@@ -13,3 +13,39 @@ export function Caption({ children, ...props }) {
   const classes = classNames("Caption", { CenterText: props.center });
   return <h2 className={classes}>{children}</h2>;
 }
+
+export function Field({ name, label, type, register }) {
+  if (type === "checkbox") {
+    return (
+      <InputGroup>
+        <Label className="Inline">
+          <input
+            className="Input Inline"
+            name={name}
+            ref={register()}
+            type={type}
+          />
+          {label}
+        </Label>
+      </InputGroup>
+    );
+  }
+  return (
+    <InputGroup>
+      {label && <Label>{label}</Label>}
+      <input
+        className="Input"
+        name={name}
+        ref={register()}
+        type={type || "text"}
+      />
+    </InputGroup>
+  );
+}
+
+export function InputGroup({ children }) {
+  return <div className="InputGroup">{children}</div>;
+}
+export function Label({ children, className }) {
+  return <label className={`Label ${className || ""}`}>{children}</label>;
+}
