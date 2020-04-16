608730e5b23c3ab9159b4c9e4eb1c86a51c2397a
Kiss György <kissgyorgy@me.com>
2020-04-16 13:32:04 +0200

    Revert "Add initail start payment view"
    This reverts commit bb2fcd03c346079982afbf484ba1d37540b78785.
    
    This will be implemented later, we start with on-site payment only.

--- code/backend/payments/views.py
+++ code/backend/payments/views.py
@@ -1,11 +1,7 @@
-from django.conf import settings
-from django.shortcuts import get_object_or_404, render
-from online_payments import simple_v2
 from rest_framework import viewsets
 
 from . import models as m
 from . import serializers as s
-from . import utils as u
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -16,18 +12,3 @@ class PaymentViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = m.Transaction.objects.all()
     serializer_class = s.TransactionSerializer
-
-
-def initiate_online_payment_view(request, appointment_pk):
-    appointment = get_object_or_404(m.Appointment, pk=appointment_pk)
-    payments, transactions = u.create_payments_and_transactions_for_appointment(appointment)
-    transaction = transactions[0]
-    simple_response = simple_v2.start_payment_request(
-        merchant=settings.SIMPLEPAY_MERCHANT_ID,
-        secret_key=settings.SIMPLEPAY_SECRET_KEY,
-        customer_email=appointment.email,
-        order_ref=transaction.pk,
-        total=transaction.amount,
-    )
-    context = {"simple_url": simple_response.payment_url}
-    return render(request, "simple-post-form.html", context=context)
