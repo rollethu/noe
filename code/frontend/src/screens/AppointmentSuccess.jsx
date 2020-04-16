5b2fb2a5233acbf08aeef116520bd59ed03e5287
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 22:47:34 +0200

    Add AppointmentSuccess Screen

--- code/frontend/src/screens/AppointmentSuccess.jsx
+++ code/frontend/src/screens/AppointmentSuccess.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-import { View, Caption } from "../UI";
+import { View, Caption, Text, Image, Button } from "../UI";
 
 export default function AppointmentSuccess() {
   return (
     <View>
-      <Caption>AppointmentSuccess</Caption>
+      <Caption center>Sikeres Regisztráció</Caption>
+      <Image src="https://via.placeholder.com/150" />
+      <Text>
+        Időpontfoglalása rögzítésre került. A regisztráció részleteit és további
+        információt a megadott e-mail címre küldött üzenetben talál. Kérjük
+        időben érkezzen a csúszások elkerülése végett.
+      </Text>
+      <Button toCenter>Rendben</Button>
     </View>
   );
 }
