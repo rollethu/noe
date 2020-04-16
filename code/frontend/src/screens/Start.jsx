9e6125237de97b5dd1b0d19223ce81b1e0ff3215
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 14:19:45 +0200

    Add start form without submission

--- code/frontend/src/screens/Start.jsx
+++ code/frontend/src/screens/Start.jsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
-import { View, Caption } from "../UI";
+import { View, Caption, Form, Field, Button } from "../UI";
 
-const TXT_CAPTION_TEXT = "Áthajtásos koronavírus (COVID-19) teszt";
+const TXT_CAPTION = "Áthajtásos koronavírus (COVID-19) teszt";
+const TXT_SUBMIT_BUTTON = "Tovább";
+const TXT_ACCEPT_GTC = "Elfogadom az ÁSZF-et.";
+const TXT_ACCEPT_PRIVACY_POLICY = "Elfogadom az Adatvédelmi Szabályzatot.";
 
 export default function Start() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <View>
-      <Caption center>{TXT_CAPTION_TEXT}</Caption>
+      <Caption center>{TXT_CAPTION}</Caption>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Field register={register} name="email" label="E-mail" type="text" />
+        <Field
+          register={register}
+          name="gtc"
+          label={TXT_ACCEPT_GTC}
+          type="checkbox"
+        />
+        <Field
+          register={register}
+          name="privacy_policy"
+          label={TXT_ACCEPT_PRIVACY_POLICY}
+          type="checkbox"
+        />
+        <Button type="submit">{TXT_SUBMIT_BUTTON}</Button>
+      </Form>
     </View>
   );
 }
