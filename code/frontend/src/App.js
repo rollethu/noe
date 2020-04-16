9a1286974cb62aacde36a2405c035cf8883aec72
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 16:40:47 +0200

    Move routes to constants
    This way we can avoid typos, e.g. when redirecting

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -20,18 +20,29 @@ import Time from "./screens/Time";
 import Checkout from "./screens/Checkout";
 import AppointmentSuccess from "./screens/AppointmentSuccess";
 
+export const ROUTE_START = "/start";
+export const ROUTE_EMAIL_VERIFICATION = "/email-megerosites";
+export const ROUTE_REGISTRATION = "/regisztracio";
+export const ROUTE_SEAT_DETAILS = "/szemelyes-adatok";
+export const ROUTE_SURVEY = "/kerdoiv";
+export const ROUTE_ADD_SEAT = "/uj-szemely";
+export const ROUTE_TIME = "/idopont";
+export const ROUTE_PAYMENT_METHOD = "/fizetesi-mod";
+export const ROUTE_CHEKCOUT = "/osszegzes";
+export const ROUTE_APPOINTMENT_SUCCESS = "/sikeres-regisztracio";
+
 function App() {
   const routes = [
-    { path: "/start", component: Start },
-    { path: "/email-megerosites", component: EmailVerification },
-    { path: "/regisztracio", component: Registration },
-    { path: "/szemelyes-adatok", component: SeatDetails },
-    { path: "/kerdoiv", component: Survey },
-    { path: "/uj-szemely", component: AddSeat },
-    { path: "/idopont", component: Time },
-    { path: "/fizetesi-mod", component: PaymentMethod },
-    { path: "/osszegzes", component: Checkout },
-    { path: "/sikeres-regisztracio", component: AppointmentSuccess },
+    { path: ROUTE_START, component: Start },
+    { path: ROUTE_EMAIL_VERIFICATION, component: EmailVerification },
+    { path: ROUTE_REGISTRATION, component: Registration },
+    { path: ROUTE_SEAT_DETAILS, component: SeatDetails },
+    { path: ROUTE_SURVEY, component: Survey },
+    { path: ROUTE_ADD_SEAT, component: AddSeat },
+    { path: ROUTE_TIME, component: Time },
+    { path: ROUTE_PAYMENT_METHOD, component: PaymentMethod },
+    { path: ROUTE_CHEKCOUT, component: Checkout },
+    { path: ROUTE_APPOINTMENT_SUCCESS, component: AppointmentSuccess },
   ];
   return (
     <div className="App">
