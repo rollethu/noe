28b802219adcc975083ad57d23c6164be76152f0
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 13:33:45 +0200

    Move text out from component

--- code/frontend/src/screens/Start.jsx
+++ code/frontend/src/screens/Start.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { View, Caption } from "../UI";
 
+const TXT_CAPTION_TEXT = "Áthajtásos koronavírus (COVID-19) teszt";
+
 export default function Start() {
   return (
     <View>
-      <Caption>Start</Caption>
+      <Caption>{TXT_CAPTION_TEXT}</Caption>
     </View>
   );
 }
