beffc277f6d36b7adb52a114befb05140e0ff44a
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-16 13:23:12 +0200

    Refactor routes and add helper nav for dev

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles/styles.css";
+
+import Nav from "./components/Nav";
 import Start from "./screens/Start";
 import AddSeat from "./screens/AddSeat";
 import EmailVerification from "./screens/EmailVerification";
@@ -8,40 +10,31 @@ import Registration from "./screens/Registration";
 import SeatDetails from "./screens/SeatDetails";
 import Survey from "./screens/Survey";
 import Time from "./screens/Time";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AppointmentSuccess from "./screens/AppointmentSuccess";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const routes = [
+    { path: "/start", component: Start },
+    { path: "/email-megerosites", component: EmailVerification },
+    { path: "/regisztracio", component: Registration },
+    { path: "/uj-szemely", component: AddSeat },
+    { path: "/fizetesi-mod", component: PaymentMethod },
+    { path: "/szemelyes-adatok", component: SeatDetails },
+    { path: "/kerdoiv", component: Survey },
+    { path: "/idopont", component: Time },
+    { path: "/sikeres-regisztracio", component: AppointmentSuccess },
+  ];
   return (
     <div className="App">
       <Router>
+        <Nav routes={routes} />
         <Switch>
-          <Route path="/start">
-            <Start />
-          </Route>
-          <Route path="/email-megerosites">
-            <EmailVerification />
-          </Route>
-          <Route path="/regisztracio">
-            <Registration />
-          </Route>
-          <Route path="/szemelyes-adatok">
-            <SeatDetails />
-          </Route>
-          <Route path="/kerdoiv">
-            <Survey />
-          </Route>
-          <Route path="/uj-szemely">
-            <AddSeat />
-          </Route>
-          <Route path="/idopont">
-            <Time />
-          </Route>
-          <Route path="/fizetes-mod">
-            <PaymentMethod />
-          </Route>
-          <Route path="/sikeres-regisztracio">
-            <Start />
-          </Route>
+          {routes.map((route) => (
+            <Route path={route.path} key={route.path}>
+              <route.component></route.component>
+            </Route>
+          ))}
         </Switch>
       </Router>
     </div>
---
+++ code/frontend/src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+let defaultShowNav = false;
+if (process.env.NODE_ENV === "development") {
+  defaultShowNav = true;
+}
+
+export default function Nav({ routes }) {
+  const [showNav, setShowNav] = React.useState(defaultShowNav);
+
+  if (!showNav) {
+    return null;
+  }
+
+  return (
+    <div className="Nav">
+      <button onClick={() => setShowNav(false)}>Close</button>
+      <ul>
+        {routes.map((route) => (
+          <Link to={route.path}>
+            <li>{route.path}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
