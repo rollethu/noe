aa43786a4de11ce5bcbf64fd6363961b22fe353e
Kiss György <kissgyorgy@me.com>
2020-04-19 00:13:43 +0200

    Improve SurveyQuestion admin

--- code/backend/surveys/admin.py
+++ code/backend/surveys/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from . import models as m
 
 
-admin.site.register(m.SurveyQuestion)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ["question", "answer_datatype", "is_required", "is_active"]
+
+
+admin.site.register(m.SurveyQuestion, SurveyQuestionAdmin)
 admin.site.register(m.SurveyAnswer)
