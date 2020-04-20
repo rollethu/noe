0fdeb33af5c6dd79117d0010b801b7fd13a0152f
daniel.meszaros <daniel.meszaros@rollet.app>
2020-04-20 23:55:18 +0200

    added staging config + refactor fab

--- code/frontend/fabfile/deployment.py
+++ code/frontend/fabfile/deployment.py
@@ -4,9 +4,24 @@ import datetime as dt
 import boto3
 from fabric.api import task, lcd, local, run
 
+settings = {
+    "staging": {
+        "node_env" : "staging",
+        "backend_host" : "https://api.noe.rollet.app",
+        "s3_bucket" : "noe.rollet.app",
+        "cf_distribution": "EDEEHWJ4WTQG"
+    },
+    "production" : {
+        "node_env" : "production",
+        "backend_host" : "https://api.tesztallomas.hu",
+        "s3_bucket" : "regisztracio.tesztallomas.hu",
+        "cf_distribution" : "E3HVIVZU5PTARC"
+    }
+}
 
 @task
-def build():
+def build(environment='staging'):
+    
     local("docker build -t project-noe-frontend:latest .")
     try:
         local("docker rm -f temp-project-noe-frontend")
@@ -18,7 +33,8 @@ def build():
         "-v ${PWD}/src:/project-noe/frontend/src "
         "-v ${PWD}/public:/project-noe/frontend/public "
         "-v ${PWD}/package.json:/project-noe/frontend/package.json "
-        "-e REACT_APP_BACKEND_HOST=https://api.tesztallomas.hu "
+        f"-e REACT_APP_BACKEND_HOST={settings[environment]['backend_host']} "
+        f"-e REACT_APP_NODE_ENV={settings[environment]['node_env']} "
         "project-noe-frontend:latest "
         "yarn build"
     )
@@ -27,10 +43,10 @@ def build():
 
 
 @task
-def deploy():
+def deploy(environment='staging'):
     local("pwd")
     local("ls -la")
-    local("aws s3 sync temp_build s3://noe.rollet.app")
+    local(f"aws s3 sync temp_build s3://{settings[environment]['s3_bucket']}")
     local("rm -rf temp_build")
 
     client = boto3.client("cloudfront")
@@ -42,11 +58,10 @@ def deploy():
         "/manifest.json",
         "/app-icon.png",
     ]
-    for distribution_id in os.environ.get("AWS_CLOUDFRONT_DISTRIBUTION_IDS", "").split(","):
-        client.create_invalidation(
-            DistributionId=distribution_id.strip(),
-            InvalidationBatch={
-                "Paths": {"Quantity": len(items_to_invalidate), "Items": items_to_invalidate},
-                "CallerReference": dt.datetime.now().strftime("%Y%m%d%H%M%S"),
-            },
-        )
+    client.create_invalidation(
+        DistributionId=settings[environment]['cf_distribution'],
+        InvalidationBatch={
+            "Paths": {"Quantity": len(items_to_invalidate), "Items": items_to_invalidate},
+            "CallerReference": dt.datetime.now().strftime("%Y%m%d%H%M%S"),
+        },
+    )
