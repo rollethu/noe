e3c6673a817bcfaf96b16c840a2b4ab7fa258a21
Kiss György <kissgyorgy@me.com>
2020-04-20 23:24:49 +0200

    Add permission handling to /staff-api/
    Only logged in and is_staff (currently = is_admin) users are allowed.
    Protect the Root Api endpoint too.

--- code/backend/staff_api/views.py
+++ code/backend/staff_api/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse as django_reverse
 from django.shortcuts import redirect
 from rest_framework import routers, viewsets
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from appointments.models import Appointment, Seat
 from payments.models import Payment
 from . import serializers as s
@@ -13,14 +14,23 @@ class StaffAPIRoot(routers.APIRootView):
     REST API endpoints for staff members.
     """
 
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_staff:
+            raise PermissionDenied()
+        return super().get(request, *args, **kwargs)
 
-class AppointmentViewSet(viewsets.ModelViewSet):
+
+class _StaffViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+
+class AppointmentViewSet(_StaffViewSet):
     queryset = Appointment.objects.all()
     serializer_class = s.AppointmentSerializer
     filterset_class = f.AppointmentFilter
 
 
-class SeatViewSet(viewsets.ModelViewSet):
+class SeatViewSet(_StaffViewSet):
     queryset = Seat.objects.all()
     serializer_class = s.SeatSerializer
 
@@ -30,6 +40,6 @@ class SeatViewSet(viewsets.ModelViewSet):
         return super().retrieve(request)
 
 
-class PaymentViewSet(viewsets.ModelViewSet):
+class PaymentViewSet(_StaffViewSet):
     queryset = Payment.objects.all()
     serializer_class = s.PaymentSerializer
