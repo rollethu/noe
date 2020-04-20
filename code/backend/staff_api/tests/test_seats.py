474aec40b319d5b5823337d7d6d5d010d9b1a828
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-20 18:06:43 +0200

    Redirect Sample to django admin without custom header

---
+++ code/backend/staff_api/tests/test_seats.py
@@ -0,0 +1,16 @@
+import pytest
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+
+@pytest.mark.django_db
+def test_seat_detail_url_redirect_to_admin_without_header(api_client, seat):
+    rv = api_client.get(reverse("staff-seat-detail", kwargs={"pk": seat.pk}))
+    assert rv.status_code == status.HTTP_302_FOUND
+    assert rv["location"] == reverse("admin:appointments_seat_change", kwargs={"object_id": seat.pk})
+
+
+@pytest.mark.django_db
+def test_seat_detail_returns_response_with_header(api_client, seat):
+    rv = api_client.get(reverse("staff-seat-detail", kwargs={"pk": seat.pk}), HTTP_USE_STAFF_API="true")
+    assert rv.status_code == status.HTTP_200_OK
--- code/backend/staff_api/views.py
+++ code/backend/staff_api/views.py
@@ -1,4 +1,7 @@
+from django.urls import reverse as django_reverse
+from django.shortcuts import redirect
 from rest_framework import routers, viewsets
+
 from appointments.models import Appointment, Seat
 from payments.models import Payment
 from . import serializers as s
@@ -18,6 +21,11 @@ class AppointmentViewSet(viewsets.ModelViewSet):
 class SeatViewSet(viewsets.ModelViewSet):
     queryset = Seat.objects.all()
     serializer_class = s.SeatSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        if "USE-STAFF-API" not in request.headers:
+            return redirect(django_reverse("admin:appointments_seat_change", kwargs={"object_id": kwargs["pk"]}))
+        return super().retrieve(request)
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
