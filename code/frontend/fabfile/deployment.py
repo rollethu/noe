73b2a71071d6cae6557006a9d100d00d2593a94e
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-20 15:32:10 +0200

    Format deployment.py in fronted (Black)

--- code/frontend/fabfile/deployment.py
+++ code/frontend/fabfile/deployment.py
@@ -7,47 +7,46 @@ from fabric.api import task, lcd, local, run
 
 @task
 def build():
-    local('docker build -t project-noe-frontend:latest .')
+    local("docker build -t project-noe-frontend:latest .")
     try:
-        local('docker rm -f temp-project-noe-frontend')
+        local("docker rm -f temp-project-noe-frontend")
     except:
         pass
-    local('docker run '
-          '--name temp-project-noe-frontend '
-          '-v ${PWD}/src:/project-noe/frontend/src '
-          '-v ${PWD}/public:/project-noe/frontend/public '
-          '-v ${PWD}/package.json:/project-noe/frontend/package.json '
-          '-e BACKEND_HOST=https://api.tesztallomas.hu '
-          'project-noe-frontend:latest '
-          'yarn build')
-    local('docker cp temp-project-noe-frontend:project-noe/frontend/build temp_build')
-    local('docker rm -f temp-project-noe-frontend')
+    local(
+        "docker run "
+        "--name temp-project-noe-frontend "
+        "-v ${PWD}/src:/project-noe/frontend/src "
+        "-v ${PWD}/public:/project-noe/frontend/public "
+        "-v ${PWD}/package.json:/project-noe/frontend/package.json "
+        "-e BACKEND_HOST=https://api.tesztallomas.hu "
+        "project-noe-frontend:latest "
+        "yarn build"
+    )
+    local("docker cp temp-project-noe-frontend:project-noe/frontend/build temp_build")
+    local("docker rm -f temp-project-noe-frontend")
 
 
 @task
 def deploy():
-    local('pwd')
-    local('ls -la')
-    local('aws s3 sync temp_build s3://noe.rollet.app')
-    local('rm -rf temp_build')
+    local("pwd")
+    local("ls -la")
+    local("aws s3 sync temp_build s3://noe.rollet.app")
+    local("rm -rf temp_build")
 
-    client = boto3.client('cloudfront')
+    client = boto3.client("cloudfront")
     items_to_invalidate = [
-        '/',
-        '/static/css/styles.sass',
-        '/index.html',
-        '/favicon.ico',
-        '/manifest.json',
-        '/app-icon.png',
+        "/",
+        "/static/css/styles.sass",
+        "/index.html",
+        "/favicon.ico",
+        "/manifest.json",
+        "/app-icon.png",
     ]
-    for distribution_id in os.environ.get('AWS_CLOUDFRONT_DISTRIBUTION_IDS', '').split(','):
+    for distribution_id in os.environ.get("AWS_CLOUDFRONT_DISTRIBUTION_IDS", "").split(","):
         client.create_invalidation(
             DistributionId=distribution_id.strip(),
             InvalidationBatch={
-                'Paths': {
-                    'Quantity': len(items_to_invalidate),
-                    'Items': items_to_invalidate
-                },
-                'CallerReference': dt.datetime.now().strftime('%Y%m%d%H%M%S')
-            }
-    )
+                "Paths": {"Quantity": len(items_to_invalidate), "Items": items_to_invalidate},
+                "CallerReference": dt.datetime.now().strftime("%Y%m%d%H%M%S"),
+            },
+        )
