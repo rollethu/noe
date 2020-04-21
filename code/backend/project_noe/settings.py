9be1142071bfe9737dbdeb08bdb4851af11e5585
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-21 11:38:35 +0200

    Get DEBUG from environment

--- code/backend/project_noe/settings.py
+++ code/backend/project_noe/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
