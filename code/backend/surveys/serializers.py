064839160cfcce403279e3fd0c2c8369db68dd5e
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-23 14:31:20 +0200

    Handle multiple survey errors

--- code/backend/surveys/serializers.py
+++ code/backend/surveys/serializers.py
@@ -1,3 +1,7 @@
+from collections import ChainMap
+
+from django.utils.translation import gettext as _
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 
 from . import models as m
@@ -9,7 +13,43 @@ class SurveyQuestionSerializer(serializers.HyperlinkedModelSerializer):
         fields = "__all__"
 
 
+class SurveyAnswerListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        answers = make_all(validated_data)
+        return m.SurveyAnswer.objects.bulk_create(answers)
+
+    def make_all(self, validated_data):
+        answers = []
+        errors = []
+        for i, item in enumerate(validated_data):
+            try:
+                answers.append(self.make_one(i, item))
+            except ValidationError as error:
+                errors.append(error)
+
+        if errors:
+            raise ValidationError(dict(ChainMap(*[e.detail for e in errors])))
+
+        return answers
+
+    def make_one(self, i, validated_data):
+        self.validate_required_question(i, validated_data)
+        return m.SurveyAnswer(**validated_data)
+
+    def update(self, instance, validated_data):
+        self.validate_required_question(validated_data)
+        return super().update(instance, validated_data)
+
+    def validate_required_question(self, i, validated_data):
+        question = validated_data["question"]
+        answer = validated_data["answer"]
+
+        if question.is_required and answer in ["", None]:
+            raise ValidationError({self.initial_data[i]["question"]: _("This field is required.")})
+
+
 class SurveyAnswerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = m.SurveyAnswer
         fields = "__all__"
+        list_serializer_class = SurveyAnswerListSerializer
--- code/backend/surveys/tests/test_survey_answers.py
+++ code/backend/surveys/tests/test_survey_answers.py
@@ -46,3 +46,27 @@ def test_create_multiple_answers(api_client, survey_question, survey_question2, 
     assert second_answer.question == survey_question2
     assert second_answer.seat == seat
     assert second_answer.answer == "Test answer 2"
+
+
+@pytest.mark.django_db
+def test_answer_for_required_question(api_client, survey_question, survey_question2, seat):
+    question1_url = reverse("surveyquestion-detail", kwargs={"pk": survey_question.pk})
+    question2_url = reverse("surveyquestion-detail", kwargs={"pk": survey_question2.pk})
+    seat_url = reverse("seat-detail", kwargs={"pk": seat.pk})
+
+    survey_question.is_required = True
+    survey_question2.is_required = True
+    survey_question.save()
+    survey_question2.save()
+
+    rv = api_client.post(
+        reverse("surveyanswer-list"),
+        [
+            {"question": question1_url, "seat": seat_url, "answer": ""},
+            {"question": question2_url, "seat": seat_url, "answer": ""},
+        ],
+        format="json",
+    )
+    assert rv.status_code == status.HTTP_400_BAD_REQUEST
+    assert question1_url in rv.data
+    assert question2_url in rv.data
