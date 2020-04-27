1914e62010503ed65ea5366f7ff1a93822d15caf
Kiss György <kissgyorgy@me.com>
2020-04-27 17:54:43 +0200

    Don't load environment files in container
    ignore .env file and don't even run load_debug when DEBUG = True

--- code/backend/project_noe/settings.py
+++ code/backend/project_noe/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from dotenv import load_dotenv
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
+if DEBUG:
+    from dotenv import load_dotenv
 
-load_dotenv()
-
+    load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,9 +29,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
