06c2e15173a7c774dd2b4148fe2b8505b2ab36af
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-27 13:49:27 +0200

    Clean up total price display function

--- code/frontend/src/screens/PaymentMethods/utils.js
+++ code/frontend/src/screens/PaymentMethods/utils.js
@@ -8,15 +8,14 @@ export function makePaymentUpdateRequest(appointment) {
 }
 
 export function getTotalPriceDisplay(appointment) {
-  let total = "Ár nem elérhető!";
-  if (
-    appointment.total_price !== undefined &&
-    appointment.currency !== undefined
-  ) {
-    const currency =
-      appointment.currency === "HUF" ? "Ft" : appointment.currency;
-    total = `${appointment.total_price} ${currency}`;
+  if (!appointment.currency) {
+    return "Ár nem elérhető!";
   }
 
-  return total;
+  let currency = appointment.currency;
+  if (currency === "HUF") {
+    currency = "Ft";
+  }
+
+  return `${appointment.total_price} ${currency}`;
 }
