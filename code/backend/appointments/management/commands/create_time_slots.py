0b0048dcde3d9759194a1819c96f12e668cb39da
Kiss György <kissgyorgy@me.com>
2020-04-27 16:34:48 +0200

    create_time_slots command expects location_pk

--- code/backend/appointments/management/commands/create_time_slots.py
+++ code/backend/appointments/management/commands/create_time_slots.py
@@ -9,17 +9,17 @@ class Command(BaseCommand):
     help = "Create timeslots for the ONLY location currently available"
 
     def add_arguments(self, parser):
+        parser.add_argument("location_pk", type=int)
         parser.add_argument("start", type=dt.datetime.fromisoformat)
         parser.add_argument("end", type=dt.datetime.fromisoformat)
         parser.add_argument("duration", type=int)
         parser.add_argument("capacity", type=int)
 
     def handle(self, *args, **options):
-        self.stdout.write(str(type(options["start"])) + str(options["start"]))
         start = timezone.make_aware(options["start"])
         end = timezone.make_aware(options["end"])
         duration = dt.timedelta(minutes=options["duration"])
-        location = Location.objects.get()
+        location = Location.objects.get(pk=options["location_pk"])
 
         self.stdout.write(f"Creating TimeSlots with start={start} end={end}")
         time_slots = TimeSlot.objects.create_time_slots(
