d0b75ca4e2e775f7829e7415d0a512dff89f17b8
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-04-28 11:51:11 +0200

    Allow `note` to be edited on Payments in admin, once `paid_at` is set

--- code/backend/payments/admin.py
+++ code/backend/payments/admin.py
@@ -6,6 +6,14 @@ from . import models as m
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ["uuid", "amount", "simplepay_transaction_id", "currency", "payment_method_type", "paid_at"]
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.paid_at is not None:
+            editable_fields = ["note"]
+            readonly_fields = [f.name for f in obj._meta.fields if f.name not in editable_fields]
+            return readonly_fields
+
+        return []
+
 
 class InlinePaymentAdmin(admin.TabularInline):
     model = m.Payment
