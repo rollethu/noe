72c29f5bc03100050ee8607f6f79ce488ffd13d8
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-04 11:05:26 +0200

    Finish AppointmentAuthentication

--- code/backend/appointments/auth.py
+++ code/backend/appointments/auth.py
@@ -1,7 +1,27 @@
 from rest_framework import authentication
+from rest_framework.exceptions import AuthenticationFailed
+from . import models as m
 
 
 class AppointmentAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         # Authorization: Apptoken <hashed value of Appointment UUID>
         auth_header = request.META.get("HTTP_AUTHORIZATION")
+        if auth_header is None:
+            return None
+
+        scheme, token = auth_header.split(" ")
+
+        if scheme != "Apptoken":
+            raise AuthenticationFailed()
+
+        try:
+            ev = m.EmailVerification.objects.select_realted("appointment").get_by_token(token)
+        except m.EmailVerification.DoesNotExist:
+            raise AuthenticationFailed()
+
+        if ev.verified_at is None:
+            raise AuthenticationFailed()
+
+        request.appointment = ev.appointment
+        return (request.appointment, None)
