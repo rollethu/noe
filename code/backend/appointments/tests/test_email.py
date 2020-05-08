470706cdfe70b7a3197a573d0aa3878a152a5523
Kiss György <kissgyorgy@me.com>
2020-05-08 10:35:20 +0200

    Changed email template
    - Include product type
    - Link to "Hozzájárulási nyilatkozat"
    - Information about test results dynamically
    - URL with https scheme (WIX is faster to load this way,
      because there are less redirects)
    - New test for priority pricing

--- code/backend/appointments/tests/test_email.py
+++ code/backend/appointments/tests/test_email.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.utils import timezone
 import pytest
 from payments.models import Payment
-from payments.prices import PaymentMethodType
+from payments.prices import PaymentMethodType, ProductType
 from .. import email
 from .. import models as m
 
@@ -37,13 +37,16 @@ def test_send_qrcode():
     appointment = m.Appointment(
         start=dt.datetime(2020, 4, 24, 9, 10), location=location, normalized_licence_plate=licence_plate
     )
+    payment = Payment(
+        amount=26_990, payment_method_type=PaymentMethodType.ON_SITE, product_type=ProductType.NORMAL_EXAM,
+    )
     seat = m.Seat(
         full_name=full_name,
         birth_date=timezone.now(),
         email="test@rollet.app",
         appointment=appointment,
         qrcode=m.QRCode(code=code),
-        payment=Payment(amount=26_990, payment_method_type=PaymentMethodType.ON_SITE),
+        payment=payment,
     )
 
     email.send_qrcode(seat)
@@ -56,7 +59,35 @@ def test_send_qrcode():
     assert "Helyszínen" in sent_mail.body
     assert "Test Location" in sent_mail.body
     assert code in sent_mail.body
-    assert "Fizetendő összeg: 26990 Ft" in sent_mail.body
+    assert "\nFizetendő összeg: 26990 Ft\n" in sent_mail.body
+    assert "\nVizsgálat típusa: Normál vizsgálat\n" in sent_mail.body
+    assert "A normál vizsgálatok eredményét 72 órán belül küldjük Önnek." in sent_mail.body
+
+
+def test_send_qrcode_with_priority_product():
+    location = m.Location(name="Test Location")
+    appointment = m.Appointment(
+        start=dt.datetime(2020, 4, 24, 9, 10), location=location, normalized_licence_plate="ABC123"
+    )
+    payment = Payment(
+        amount=36_990, payment_method_type=PaymentMethodType.ON_SITE, product_type=ProductType.PRIORITY_EXAM,
+    )
+    seat = m.Seat(
+        full_name="Gipsz Jakab",
+        birth_date=timezone.now(),
+        email="test@rollet.app",
+        appointment=appointment,
+        qrcode=m.QRCode(code="0123-123456-1111"),
+        payment=payment,
+    )
+
+    email.send_qrcode(seat)
+    assert len(mail.outbox) == 1
+
+    sent_mail = mail.outbox[0]
+    assert "\nFizetendő összeg: 36990 Ft\n" in sent_mail.body
+    assert "\nVizsgálat típusa: Elsőbbségi vizsgálat\n" in sent_mail.body
+    assert "Az elsőbbségi vizsgálatok eredményét a teszt elvégzését követő napon küldjük Önnek." in sent_mail.body
 
 
 def test_send_qrcode_with_doctor_referral():
