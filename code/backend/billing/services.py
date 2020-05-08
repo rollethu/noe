23c92b0d3d9944fbe4ffdd86d39f36b49533972d
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-08 14:00:53 +0200

    Remove receipt sending related code

--- code/backend/billing/services.py
+++ code/backend/billing/services.py
@@ -1,4 +1,3 @@
-
 from django.conf import settings
 from online_payments.billing.enums import Currency, VATRate
 from online_payments.billing.models import Item, Receipt, PaymentMethod, Invoice, Customer
@@ -6,42 +5,6 @@ from online_payments.billing.szamlazzhu.client import Szamlazzhu
 
 from . import models as m
 from payments.prices import PRODUCTS
-
-
-
-
-
-
-def send_bill_to_seat(seat):
-    if seat.appointment.request_vat_invoice:
-        send_invoice_to_seat(seat)
-    else:
-        send_receipt_to_seat(seat)
-
-
-def send_invoice_to_seat(seat):
-    pass
-
-
-def send_receipt_to_seat(seat):
-    product = PRODUCTS[seat.payment.product_type]
-
-    net_price = product.amount / 1.27
-    vat_value = product.amount - net_price
-    receipt_item = Item(
-        name=product.product_type,
-        quantity=str(1),
-        unit="db",
-        net_unit_price=str(net_price),
-        net_price=str(net_price),
-        vat_rate=str(27),
-        vat_value=str(vat_value),
-        gross_price=str(product.amount),
-    )
-    receipt = Receipt(items=[receipt_item], payment_method=PaymentMethod.CREDIT_CARD)
-    szamlazzhu = Szamlazzhu(invoice_agent_key=settings.SZAMLAZZHU_AGENT_KEY, currency=Currency.HUF)
-    szamlazzhu.send_receipt(receipt, prefix=settings.SZAMLAZZ_HU_RECEIPT_PREFIX)
-
 
 
 def send_invoice_to_seat(seat):
@@ -70,4 +33,9 @@ def send_invoice_to_seat(seat):
     szamlazzhu = Szamlazzhu(settings.SZAMLAZZHU_AGENT_KEY, Currency.HUF)
     szamlazzhu.send_invoice(invoice, settings.SZAMLAZZHU_INVOICE_PREFIX)
 
-    m.Bill(appointment=seat.appointment, payment=seat.payment, bill_type=m.Bill.BILL_TYPE_VAT_INVOICE, bill_id=invoice.invoice_id, )
+    m.Bill(
+        appointment=seat.appointment,
+        payment=seat.payment,
+        bill_type=m.Bill.BILL_TYPE_VAT_INVOICE,
+        bill_id=invoice.invoice_id,
+    )
