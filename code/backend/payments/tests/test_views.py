b46e276bc0b22369a86e1301139e4c46c893d113
Kiss György <kissgyorgy@me.com>
2020-05-11 16:34:14 +0200

    Fix /pay-appointment/ tests work with billing
    Make a fixture for the request body mutate that in unit tests with the
    total_price. It's essentially the same everywhere.

--- code/backend/payments/tests/test_views.py
+++ code/backend/payments/tests/test_views.py
@@ -22,6 +22,23 @@ def get_price_request(factory, appointment, appointment_url):
     return request
 
 
+# This is mutated inside tests, so we need a new instance for every test
+@pytest.fixture(scope="function")
+def pay_appointment_body(appointment_url):
+    return {
+        "appointment": appointment_url,
+        "product_type": ProductType.NORMAL_EXAM,
+        "total_price": 0,
+        "currency": "HUF",
+        "company_name": "Test Company",
+        "country": "Magyarország",
+        "address_line1": "Test Address Line 1",
+        "post_code": "1234",
+        "city": "Budapest",
+        "tax_number": "123456789",
+    }
+
+
 def _authenticate_appointment(request, appointment):
     ev = EmailVerification.objects.create(appointment=appointment)
     force_authenticate(request, token=appointment)
@@ -81,50 +98,26 @@ class TestGetPriceView:
 
 @pytest.mark.django_db
 class TestPayAppointmentView:
-    def test_cannot_pay_zero_seats(self, appointment, appointment_url, factory):
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": 0,
-                "currency": "HUF",
-            },
-        )
+    def test_cannot_pay_zero_seats(self, appointment, pay_appointment_body, factory):
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_400_BAD_REQUEST
         assert "appointment" in res.data
         _assert_payments(count=0, expected_total_price=None)
 
-    def test_seat_with_empty_email_raises_ValidationError(self, appointment_url, seat, factory, appointment):
+    def test_seat_with_empty_email_raises_ValidationError(self, pay_appointment_body, seat, factory, appointment):
         seat.email = ""
         seat.save()
-        total_price = 26_990
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_pay_one_seat(self, appointment_url, seat, factory, appointment):
+    def test_pay_one_seat(self, pay_appointment_body, seat, factory, appointment):
         total_price = 26_990
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+        pay_appointment_body["total_price"] = total_price
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_200_OK
@@ -136,20 +129,14 @@ class TestPayAppointmentView:
         assert QRCode.objects.count() == 1
         assert len(mail.outbox) == 1
 
-    def test_pay_multiple_seats_different_email(self, appointment, appointment_url, factory):
+    def test_pay_multiple_seats_different_email(self, appointment, pay_appointment_body, factory):
         Seat.objects.create(appointment=appointment, birth_date=timezone.now(), email="seat@email.com")
         Seat.objects.create(appointment=appointment, birth_date=timezone.now(), email="seat2@email.com")
 
         total_price = 26_990 * 2
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+        pay_appointment_body["total_price"] = total_price
+
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_200_OK
@@ -161,21 +148,14 @@ class TestPayAppointmentView:
         appointment.refresh_from_db()
         assert appointment.is_registration_completed is True
 
-    def test_pay_multiple_seats_same_email(self, appointment, appointment_url, factory):
+    def test_pay_multiple_seats_same_email(self, appointment, pay_appointment_body, factory):
         same_email = "test@rollet.app"
         Seat.objects.create(appointment=appointment, birth_date=timezone.now(), email=same_email)
         Seat.objects.create(appointment=appointment, birth_date=timezone.now(), email=same_email)
 
         total_price = 26_990 * 2
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+        pay_appointment_body["total_price"] = total_price
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_200_OK
@@ -187,7 +167,7 @@ class TestPayAppointmentView:
         appointment.refresh_from_db()
         assert appointment.is_registration_completed is True
 
-    def test_pay_multiple_seats_doctor_referral_only(self, appointment, appointment_url, factory):
+    def test_pay_multiple_seats_doctor_referral_only(self, appointment, pay_appointment_body, factory):
         Seat.objects.create(
             appointment=appointment, birth_date=timezone.now(), has_doctor_referral=True, email="seat@email.com"
         )
@@ -196,67 +176,39 @@ class TestPayAppointmentView:
         )
 
         total_price = 0
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+        pay_appointment_body["total_price"] = total_price
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_200_OK
         assert res.data["total_price"] == total_price
         _assert_payments(count=2, expected_total_price=total_price)
 
-    def test_cannot_pay_completed_registration(self, appointment, appointment_url, seat, factory):
+    def test_cannot_pay_completed_registration(self, appointment, pay_appointment_body, seat, factory):
         appointment.is_registration_completed = True
         appointment.save()
 
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": 0,
-                "currency": "HUF",
-            },
-        )
+        pay_appointment_body["total_price"] = 0
+
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_400_BAD_REQUEST
         assert "appointment" in res.data
         _assert_payments(count=0, expected_total_price=None)
 
-    def test_different_total_price_sent_than_calculated(self, appointment, appointment_url, seat, factory):
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": 0,
-                "currency": "HUF",
-            },
-        )
+    def test_different_total_price_sent_than_calculated(self, appointment, pay_appointment_body, seat, factory):
+        pay_appointment_body["total_price"] = 0
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_400_BAD_REQUEST
         assert "total_price" in res.data
         _assert_payments(count=0, expected_total_price=None)
 
-    def test_proper_qrcode_format(self, appointment_url, seat, factory, appointment):
-        total_price = 26_990
-        request = factory.post(
-            "/api/pay-appointment/",
-            {
-                "appointment": appointment_url,
-                "product_type": ProductType.NORMAL_EXAM,
-                "total_price": total_price,
-                "currency": "HUF",
-            },
-        )
+    def test_proper_qrcode_format(self, pay_appointment_body, seat, factory, appointment):
+        pay_appointment_body["total_price"] = 26_990
+        request = factory.post("/api/pay-appointment/", pay_appointment_body)
         _authenticate_appointment(request, appointment)
         res = pay_appointment_view(request)
         assert res.status_code == status.HTTP_200_OK
