975d9a1403921aaa40b9173839b2b759c596f0ee
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-14 13:10:30 +0200

    Adjust test to add extra context providers

--- code/frontend/src/screens/PaymentMethods/PaymentMethods.test.js
+++ code/frontend/src/screens/PaymentMethods/PaymentMethods.test.js
@@ -11,15 +11,24 @@ import { mount } from "enzyme";
 
 import { Provider as AppointmentProvider } from "../../contexts/appointmentContext";
 import { Provider as SeatProvider } from "../../contexts/seatContext";
+import { Provider as SurveyProvider } from "../../contexts/surveyContext";
+import { Provider as TimeSlotProvider } from "../../contexts/timeSlotContext";
+import { Provider as LocationProvider } from "../../contexts/locationContext";
 import PaymentMethods, { CREDIT_CARD_ONLINE } from "./PaymentMethods";
 
 test("Online Payment is the default value", () => {
   const wrapper = mount(
-    <AppointmentProvider>
-      <SeatProvider>
-        <PaymentMethods />
-      </SeatProvider>
-    </AppointmentProvider>
+    <SurveyProvider>
+      <TimeSlotProvider>
+        <LocationProvider>
+          <AppointmentProvider>
+            <SeatProvider>
+              <PaymentMethods />
+            </SeatProvider>
+          </AppointmentProvider>
+        </LocationProvider>
+      </TimeSlotProvider>
+    </SurveyProvider>
   );
   const paymentMethodField = wrapper.find('select[name="payment_method"]');
   expect(paymentMethodField.getDOMNode().value).toBe(CREDIT_CARD_ONLINE);
