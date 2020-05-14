fa5499b3b303a935489e26b85720245e89657748
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-14 14:14:20 +0200

    Add initial polling to payment status page

--- code/frontend/src/screens/PaymentStatus/PaymentStatus.jsx
+++ code/frontend/src/screens/PaymentStatus/PaymentStatus.jsx
@@ -1,6 +1,43 @@
 import React from "react";
 import { View } from "../../UI";
 
+import { Context as AppointmentContext } from "../../contexts/appointmentContext";
+import { Context as SeatContext } from "../../contexts/seatContext";
+import { Context as surveyContext } from "../../contexts/surveyContext";
+import { ROUTE_APPOINTMENT_SUCCESS } from "../../App";
+import { useHistory } from "react-router-dom";
+
 export default function PaymentStatus() {
+  const { history } = useHistory();
+  let pollId = null;
+
+  React.useEffect(() => {
+    pollId = setTimeout(() => {
+      doPoll();
+    });
+    return () => {
+      clearInterval(pollId);
+    };
+  });
+
+  async function doPoll() {
+    const response = await fetchPaymentStatus();
+
+    if (response.error) {
+      history.push("/payment-failed");
+      return;
+    }
+
+    clearInterval(pollId);
+    const { payment_status: paymentStatus } = response.data;
+    if (paymentStatus === "SUCCESS") {
+      history.push(ROUTE_APPOINTMENT_SUCCESS);
+    } else if (paymentStatus === "PENDING") {
+      return; // let the poll continue
+    } else {
+      history.push("/payment-failed");
+    }
+  }
+
   return <View>Payment Status Page</View>;
 }
