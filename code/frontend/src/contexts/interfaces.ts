dedaa476f502b26926b4cbf08f88d883e09dd31a
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-15 14:30:49 +0200

    Add appointment, seat and survey state interfaces

---
+++ code/frontend/src/contexts/interfaces.ts
@@ -0,0 +1,16 @@
+export interface AppointmentState {
+  appointment: any;
+  emailVerification: any;
+  productId: any;
+}
+
+export interface SeatState {
+  seats: any[];
+  activeSeat: any;
+}
+
+export interface SurveyState {
+  surveyQuestions: any[];
+  surveyAnswers: any[];
+  surveyAnswersForActiveSeat: any[];
+}
