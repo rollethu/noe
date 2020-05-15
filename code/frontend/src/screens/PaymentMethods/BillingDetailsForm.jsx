b653aee4e74217abd0cbd4c35ef1d3948321082e
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-15 10:22:04 +0200

    Add default country to billing details form's prefill

--- code/frontend/src/screens/PaymentMethods/BillingDetailsForm.jsx
+++ code/frontend/src/screens/PaymentMethods/BillingDetailsForm.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {
-  Form,
-  Field,
-  InputGroup,
-  Label,
-  Flex,
-  Input,
-  HelpBlock,
-  NextButton,
-  Text,
-} from "../../UI";
+import { Form, Field, InputGroup, Label, Flex, Input, HelpBlock, NextButton, Text } from "../../UI";
 
 const defaultValues = {
   country: "Magyarország",
@@ -18,15 +8,7 @@ const defaultValues = {
 
 export default function BillingDetailsForm({ onSubmit, seat }) {
   const [wasResetted, setWasResetted] = React.useState(false);
-  const {
-    register,
-    handleSubmit,
-    setError,
-    errors,
-    setValue,
-    reset,
-    watch,
-  } = useForm({
+  const { register, handleSubmit, setError, errors, setValue, reset, watch } = useForm({
     defaultValues,
   });
   const managedSubmit = handleSubmit((values) => onSubmit(values, setError));
@@ -39,7 +21,7 @@ export default function BillingDetailsForm({ onSubmit, seat }) {
 
   function prefillForm(seat) {
     setValue("company_name", seat?.full_name);
-    setValue("country", seat?.country);
+    setValue("country", seat?.country || defaultValues.country);
     setValue("post_code", seat?.post_code);
     setValue("city", seat?.city);
     setValue("address_line1", seat?.address_line1);
@@ -73,63 +55,31 @@ export default function BillingDetailsForm({ onSubmit, seat }) {
         errors={errors}
         required
       />
-      <Field
-        register={register}
-        name="country"
-        label="Ország"
-        placeholder="Magyarország"
-        errors={errors}
-        required
-      />
-      <InputGroup
-        hasError={errors.post_code || errors.city || errors.address_line1}
-      >
+      <Field register={register} name="country" label="Ország" placeholder="Magyarország" errors={errors} required />
+      <InputGroup hasError={errors.post_code || errors.city || errors.address_line1}>
         <Label>Tartózkodási cím</Label>
         <InputGroup>
           <Flex>
             <div style={{ flex: 2 }}>
-              <Input
-                register={register}
-                name="post_code"
-                placeholder="Irányítósz."
-                required
-              />
+              <Input register={register} name="post_code" placeholder="Irányítósz." required />
               {errors.post_code && (
                 <HelpBlock>
-                  {errors.post_code.type === "required"
-                    ? "Ez a mező kötelező."
-                    : errors.post_code.message}
+                  {errors.post_code.type === "required" ? "Ez a mező kötelező." : errors.post_code.message}
                 </HelpBlock>
               )}
             </div>
             <div style={{ flex: 5, marginLeft: 10 }}>
-              <Input
-                register={register}
-                name="city"
-                placeholder="Település"
-                required
-              />
+              <Input register={register} name="city" placeholder="Település" required />
               {errors.city && (
-                <HelpBlock>
-                  {errors.city.type === "required"
-                    ? "Ez a mező kötelező."
-                    : errors.city.message}
-                </HelpBlock>
+                <HelpBlock>{errors.city.type === "required" ? "Ez a mező kötelező." : errors.city.message}</HelpBlock>
               )}
             </div>
           </Flex>
         </InputGroup>
-        <Input
-          register={register}
-          name="address_line1"
-          placeholder="Utca és Házszám"
-          required
-        />
+        <Input register={register} name="address_line1" placeholder="Utca és Házszám" required />
         {errors.address_line1 && (
           <HelpBlock>
-            {errors.address_line1.type === "required"
-              ? "Ez a mező kötelező."
-              : errors.address_line1.message}
+            {errors.address_line1.type === "required" ? "Ez a mező kötelező." : errors.address_line1.message}
           </HelpBlock>
         )}
       </InputGroup>
