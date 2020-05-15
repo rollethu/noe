4e1d55a54b2cc24739dc2194a17a62afa934c3ca
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-15 23:16:56 +0200

    Specify type for storing and loading state in localstorage

--- code/frontend/src/contexts/utils.ts
+++ code/frontend/src/contexts/utils.ts
@@ -23,13 +23,13 @@ function setErrorFlagsOnResponse(response) {
 
 export function addStateToLocalStorage(states: any) {
   localStorage.clear();
-  localStorage.setItem("appointmentState", JSON.stringify(states.appointmentState));
+  localStorage.setItem("appointmentState", JSON.stringify(<AppointmentState>states.appointmentState));
   localStorage.setItem("seatState", JSON.stringify(states.seatState));
   localStorage.setItem("surveyState", JSON.stringify(states.surveyState));
 }
 
 export function loadStateFromLocalStorage(setters) {
-  const appointmentState = JSON.parse(localStorage.getItem("appointmentState") || "{}");
+  const appointmentState: AppointmentState = JSON.parse(localStorage.getItem("appointmentState") || "{}");
   setters.setAppointmentState(appointmentState);
   setters.setSeatState(JSON.parse(localStorage.getItem("seatState") || "{}"));
   setters.setSurveyState(JSON.parse(localStorage.getItem("surveyState") || "{}"));
