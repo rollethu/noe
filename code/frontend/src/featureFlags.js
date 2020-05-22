f4d2d73ea307c2ca8341654abd3bcdc10088fdef
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-22 15:45:42 +0200

    Turn off simple feature for frontend on staging

--- code/frontend/src/featureFlags.js
+++ code/frontend/src/featureFlags.js
@@ -1,4 +1,5 @@
 const isStaging = process.env.REACT_APP_NODE_ENV === "staging";
 const isProduction = process.env.NODE_ENV === "production";
 
-export const useFeatureSimplePay = !isProduction || isStaging;
+// export const useFeatureSimplePay = !isProduction || isStaging;
+export const useFeatureSimplePay = false;
