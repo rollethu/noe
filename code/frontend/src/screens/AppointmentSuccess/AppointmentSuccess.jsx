24be7abb8622715a2f36719a3e28e194360ad0fd
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-25 16:00:26 +0200

    Render simple related success conditionally when registration is completed

--- code/frontend/src/screens/AppointmentSuccess/AppointmentSuccess.jsx
+++ code/frontend/src/screens/AppointmentSuccess/AppointmentSuccess.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import AppointmentSuccessSVG from "../../assets/success.svg";
 import { View, Caption, Text, Image } from "../../UI";
@@ -7,6 +8,8 @@ import { Context as SeatContext } from "../../contexts/seatContext";
 import { Context as SurveyContext } from "../../contexts/surveyContext";
 
 export default function AppointmentSuccess() {
+  const location = useLocation();
+  const simplePayTransactionId = location?.state?.simplePayTransactionId;
   const { resetState: resetAppointmentState } = React.useContext(AppointmentContext);
   const { resetState: resetSeatState } = React.useContext(SeatContext);
   const { resetState: resetSurveyState } = React.useContext(SurveyContext);
@@ -17,13 +20,21 @@ export default function AppointmentSuccess() {
     resetSurveyState();
   }, []);
 
+  let captionText = "Sikeres regisztráció";
+  let firstText = "Időpontfoglalása rögzítésre került.";
+  if (simplePayTransactionId) {
+    captionText = "Sikeres tranzakció";
+    firstText = `SimplePay tranzakció azonosító: ${simplePayTransactionId}`;
+  }
+
   return (
     <View>
-      <Caption center>Sikeres regisztráció</Caption>
+      <Caption center>{captionText}</Caption>
       <Image src={AppointmentSuccessSVG} />
       <Text>
-        Időpontfoglalása rögzítésre került. A regisztráció részleteit és további információt a megadott e-mail címre
-        küldött üzenetben talál. Kérjük időben érkezzen a csúszások elkerülése végett.
+        {firstText}
+        <br />A regisztráció részleteit és további információt a megadott e-mail címre küldött üzenetben talál. Kérjük
+        időben érkezzen a csúszások elkerülése végett.
       </Text>
       <a href="https://tesztallomas.hu" target="_blank" className="Button ToCenter" rel="noopener noreferrer">
         Rendben
