da57cd9e8b6147f1c23d06eccfe33c7ad3bc31e4
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-25 12:45:00 +0200

    Turn on simple feature flags

--- code/frontend/src/featureFlags.js
+++ code/frontend/src/featureFlags.js
@@ -2,4 +2,4 @@ const isStaging = process.env.REACT_APP_NODE_ENV === "staging";
 const isProduction = process.env.NODE_ENV === "production";
 
 // export const useFeatureSimplePay = !isProduction || isStaging;
-export const useFeatureSimplePay = false;
+export const useFeatureSimplePay = true;
