9d1223752e80130a565aa32dacbd6e6605254e37
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-25 15:21:01 +0200

    Rename simplepayevent to match all references

--- code/frontend/src/screens/PaymentStatus/ErrorContent.tsx
+++ code/frontend/src/screens/PaymentStatus/ErrorContent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import PaymentFailedSVG from "../../assets/payment-failed.svg";
 import { Caption, Image, Text } from "../../UI";
 
-export enum SimplePayErrorType {
+export enum SimplePayEvent {
   FAIL = "FAIL",
   TIMEOUT = "TIMEOUT",
   CANCEL = "CANCEL",
@@ -15,22 +15,22 @@ interface SimplePayError {
 }
 
 type ErrorContentProps = {
-  errorType: SimplePayErrorType;
+  errorType: SimplePayEvent;
 };
 
 export default function ErrorContent({ errorType }: ErrorContentProps) {
   const simplePayError: SimplePayError = { caption: "", errorMessage: "" };
   switch (errorType) {
-    case SimplePayErrorType.FAIL:
+    case SimplePayEvent.FAIL:
       simplePayError.caption = "Sikertelen fizetés";
       simplePayError.errorMessage = "";
       break;
-    case SimplePayErrorType.TIMEOUT:
+    case SimplePayEvent.TIMEOUT:
       simplePayError.caption = "Időtúllépés";
       simplePayError.errorMessage =
         "Ön túllépte a tranzakció elindításának lehetséges maximális idejét. A ‘Vissza’ gombra tappolva visszairányítjuk a Fizetési mód kiválasztásához.";
       break;
-    case SimplePayErrorType.CANCEL:
+    case SimplePayEvent.CANCEL:
       simplePayError.caption = "Megszakított fizetés";
       simplePayError.errorMessage =
         "Ön megszakította a fizetést. A ‘Vissza’ gombra tappolva visszairányítjuk a Fizetési mód kiválasztásához.";
--- code/frontend/src/screens/PaymentStatus/PaymentFailed.tsx
+++ code/frontend/src/screens/PaymentStatus/PaymentFailed.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import { View, LinkButton } from "../../UI";
 import { ROUTE_PAYMENT_METHODS } from "../../App";
-import ErrorContent, { SimplePayErrorType } from "./ErrorContent";
+import ErrorContent, { SimplePayEvent } from "./ErrorContent";
 
 export default function PaymentFailed(props) {
-  const errorType = props.location.search.error_type as SimplePayErrorType;
+  const errorType = props.location.search.error_type as SimplePayEvent;
   return (
     <View>
       <ErrorContent errorType={errorType} />
