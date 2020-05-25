79f09303e2802ee20895c442e20cc7bebe339e48
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-25 15:59:35 +0200

    Move simple related query param keys to consts file

--- code/frontend/src/screens/PaymentStatus/PaymentFailed.tsx
+++ code/frontend/src/screens/PaymentStatus/PaymentFailed.tsx
@@ -4,17 +4,14 @@ import { useLocation } from "react-router-dom";
 import { View, LinkButton } from "../../UI";
 import { ROUTE_PAYMENT_METHODS } from "../../App";
 import ErrorContent, { SimplePayEvent } from "./ErrorContent";
-
-const SIMPLEPAY_TRANSACTION_EVENT = "simplepay_transaction_event";
-const SIMPLEPAY_TRANSACTION_ID = "simplepay_transaction_id";
+import * as consts from "./consts";
 
 export default function PaymentFailed(props) {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
 
-  const simplePayEvent = params.get(SIMPLEPAY_TRANSACTION_EVENT) as SimplePayEvent;
-  const simplePayTransactionId = params.get(SIMPLEPAY_TRANSACTION_ID) as string;
-
+  const simplePayEvent = params.get(consts.SIMPLEPAY_TRANSACTION_EVENT) as SimplePayEvent;
+  const simplePayTransactionId = params.get(consts.SIMPLEPAY_TRANSACTION_ID) as string;
   return (
     <View>
       <ErrorContent simplePayEvent={simplePayEvent} simplePayTransactionId={simplePayTransactionId} />
---
+++ code/frontend/src/screens/PaymentStatus/consts.ts
@@ -0,0 +1,2 @@
+export const SIMPLEPAY_TRANSACTION_EVENT = "simplepay_transaction_event";
+export const SIMPLEPAY_TRANSACTION_ID = "simplepay_transaction_id";
