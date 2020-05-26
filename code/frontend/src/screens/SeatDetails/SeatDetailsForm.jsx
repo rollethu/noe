0f684864b3834e76ba68e1eb7005e716cebd69c7
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-05-26 17:04:20 +0200

    Reformat

--- code/frontend/src/screens/SeatDetails/SeatDetailsForm.jsx
+++ code/frontend/src/screens/SeatDetails/SeatDetailsForm.jsx
@@ -4,16 +4,7 @@ import { useHistory, useLocation } from "react-router-dom";
 
 import * as utils from "../../utils";
 import { ROUTE_SURVEY as redirectRoute } from "../../App";
-import {
-  InputGroup,
-  Label,
-  Input,
-  Form,
-  Field,
-  NextButton,
-  Flex,
-  HelpBlock,
-} from "../../UI";
+import { InputGroup, Label, Input, Form, Field, NextButton, Flex, HelpBlock } from "../../UI";
 import { Context as SeatContext } from "../../contexts/seatContext";
 import { Context as AppointmentContext } from "../../contexts/appointmentContext";
 
@@ -31,8 +22,7 @@ export default function SeatDetailsForm() {
   const {
     state: { appointment },
   } = React.useContext(AppointmentContext);
-  const submitMode =
-    activeSeat === null ? SUBMIT_MODE_CREATE : SUBMIT_MODE_UPDATE;
+  const submitMode = activeSeat === null ? SUBMIT_MODE_CREATE : SUBMIT_MODE_UPDATE;
   const { register, handleSubmit, setError, errors, watch } = useForm({
     defaultValues:
       submitMode === SUBMIT_MODE_CREATE
@@ -80,20 +70,8 @@ export default function SeatDetailsForm() {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <Field
-        register={register}
-        name="full_name"
-        label="Név"
-        errors={errors}
-        placeholder="Példa Péter"
-      />
-      <Field
-        register={register}
-        name="birth_date"
-        label="Születési dátum"
-        type="date"
-        errors={errors}
-      />
+      <Field register={register} name="full_name" label="Név" errors={errors} placeholder="Példa Péter" />
+      <Field register={register} name="birth_date" label="Születési dátum" type="date" errors={errors} />
       <Field
         register={register}
         name="identity_card_number"
@@ -102,21 +80,13 @@ export default function SeatDetailsForm() {
         helpText="Személyigazolvány hiányában, jogosítvány vagy útlevél száma"
         placeholder="123456AB"
       />
-      <InputGroup
-        hasError={errors.post_code || errors.city || errors.address_line1}
-      >
+      <InputGroup hasError={errors.post_code || errors.city || errors.address_line1}>
         <Label>Tartózkodási cím</Label>
         <InputGroup>
           <Flex>
             <div style={{ flex: 2 }}>
-              <Input
-                register={register}
-                name="post_code"
-                placeholder="Irányítósz."
-              />
-              {errors.post_code && (
-                <HelpBlock>{errors.post_code.message}</HelpBlock>
-              )}
+              <Input register={register} name="post_code" placeholder="Irányítósz." />
+              {errors.post_code && <HelpBlock>{errors.post_code.message}</HelpBlock>}
             </div>
             <div style={{ flex: 5, marginLeft: 10 }}>
               <Input register={register} name="city" placeholder="Település" />
@@ -124,14 +94,8 @@ export default function SeatDetailsForm() {
             </div>
           </Flex>
         </InputGroup>
-        <Input
-          register={register}
-          name="address_line1"
-          placeholder="Utca és Házszám"
-        />
-        {errors.address_line1 && (
-          <HelpBlock>{errors.address_line1.message}</HelpBlock>
-        )}
+        <Input register={register} name="address_line1" placeholder="Utca és Házszám" />
+        {errors.address_line1 && <HelpBlock>{errors.address_line1.message}</HelpBlock>}
       </InputGroup>
       <Field
         register={register}
@@ -169,13 +133,7 @@ export default function SeatDetailsForm() {
           message: "Használjon 9 számjegyet.",
         }}
       />
-      <Field
-        register={register}
-        name="doctor_name"
-        label="Háziorvos neve"
-        type="string"
-        errors={errors}
-      />
+      <Field register={register} name="doctor_name" label="Háziorvos neve" type="string" errors={errors} />
       <NextButton type="submit" />
     </Form>
   );
