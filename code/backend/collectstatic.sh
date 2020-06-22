3585ecb6622f25f8d4c315fd60a1c0e050c1beca
Kiss György <kissgyorgy@me.com>
2020-06-22 17:13:12 +0200

    Fixed collectstatic.sh
    A lot of variables changed, some variables could be deleted.

--- code/backend/collectstatic.sh
+++ code/backend/collectstatic.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-export DJANGO_SECRET_KEY=doesnmatterforcollectstatic
-export ALLOWED_CORS_HOSTS=doesnmatterforcollectstatic
-export EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
-export DJANGO_EMAIL_HOST=doesnmatterforcollectstatic
-export DJANGO_EMAIL_PORT=doesnmatterforcollectstatic
-export DJANGO_EMAIL_HOST_USER=doesnmatterforcollectstatic
-export DJANGO_EMAIL_HOST_PASSWORD=doesnmatterforcollectstatic
-export DJANGO_EMAIL_USE_TLS=true
-export DJANGO_DEFAULT_FROM_EMAIL="doesnmatterforcollectstatic"
+export DJANGO_DEBUG=false
+export SECRET_KEY="doesnmatterforcollectstatic"
 export LOG_LEVEL="DEBUG"
+
+export EMAIL_BACKEND="console"
+export EMAIL_HOST="doesnmatterforcollectstatic"
+export EMAIL_DEFAULT_FROM="doesnmatterforcollectstatic"
 export EMAIL_VERIFICATION_KEY="78wdidbLyLE-Pe1LSgSru5Cqspya5hFGKtydrP-48Rc="
+
 export FRONTEND_URL="doesnmatterforcollectstatic"
 export BACKEND_URL="doesnmatterforcollectstatic"
-export DJANGO_DEBUG=true
-export DJANGO_DATABASE_ENGINE="django.db.backends.sqlite3"
-export DJANGO_DATABASE_NAME="db.sqlite3"
-export DJANGO_DATABASE_USER="doesnmatterforcollectstatic"
-export DJANGO_DATABASE_PASSWORD="doesnmatterforcollectstatic"
-export DJANGO_DATABASE_HOST="doesnmatterforcollectstatic"
-export DJANGO_DATABASE_PORT=doesnmatterforcollectstatic
+
+export DATABASE_ENGINE="sqlite"
+export DATABASE_NAME="db.sqlite3"
 
 export STATIC_ROOT=/project_noe/static_root
+export ALLOWED_CORS_HOSTS="doesnmatterforcollectstatic"
+
+export SZAMLAZZHU_AGENT_KEY="doesnmatterforcollectstatic"
+export SZAMLAZZHU_INVOICE_PREFIX="doesnmatterforcollectstatic"
+
+export SIMPLEPAY_ENVIRONMENT="sandbox"
+export SIMPLEPAY_MERCHANT="doesnmatterforcollectstatic"
+export SIMPLEPAY_SECRET_KEY="doesnmatterforcollectstatic"
+export SIMPLEPAY_IPN_URL="doesnmatterforcollectstatic"
 
 ./manage.py collectstatic --link
